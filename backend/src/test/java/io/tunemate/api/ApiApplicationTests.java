2747f3f5f4d7db63b81f722aec70a4e0c7cfea2e
Jakub <jakub.gorzkowski@student.pk.edu.pl>
2024-04-07 20:45:58 +0200

    Initial commit

---
+++ backend/src/test/java/io/tunemate/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tunemate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

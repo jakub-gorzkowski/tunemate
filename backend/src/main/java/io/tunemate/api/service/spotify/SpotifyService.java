1d3a4385d64d92eb41356277d36c081da8aaf668
Jakub <jakub.gorzkowski@student.pk.edu.pl>
2024-05-29 02:14:32 +0200

    Deleted redundant comments

--- backend/src/main/java/io/tunemate/api/service/spotify/SpotifyService.java
+++ backend/src/main/java/io/tunemate/api/service/spotify/SpotifyService.java
@@ -2,10 +2,4 @@ package io.tunemate.api.service.spotify;
 
 public interface SpotifyService {
     String getAccessToken();
-//    Artist retrieveArtist(String artistId) throws JsonProcessingException;
-//    Set<Release> retrieveArtistReleases(String artistId) throws JsonProcessingException;
-//    Set<Track> retrieveArtistTopTracks(String artistId) throws JsonProcessingException;
-//    Release retrieveRelease(String releaseId) throws JsonProcessingException;
-//    Set<Playlist> retrieveUserPlaylists(String userId) throws JsonProcessingException;
-//    Playlist retrievePlaylist(String playlistId) throws JsonProcessingException;
 }
